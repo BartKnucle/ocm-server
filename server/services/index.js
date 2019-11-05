f9a53c04571abf17e7b8b54d5502877df92bdb83
BartKnucle <sjarde@yahoo.fr>
2019-11-05 15:25:15 +0100

    Self signed ssl

--- server/services/index.js
+++ server/services/index.js
@@ -1,5 +1,7 @@
+const connections = require('./connections/service.js')
 const devices = require('./devices/service.js')
 
 module.exports = function (app) {
+  app.configure(connections)
   app.configure(devices)
 }
