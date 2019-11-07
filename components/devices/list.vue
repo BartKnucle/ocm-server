035f827c044456ed50d2c37211de74e0bd6882c2
BartKnucle <sjarde@yahoo.fr>
2019-11-07 10:12:19 +0100

    hostname, distro

--- components/devices/list.vue
+++ components/devices/list.vue
@@ -3,6 +3,12 @@
     <v-list>
       <v-list-item v-for="item in devices().data" :key="item._id">
         <v-list-item-content>
+          {{ item.hostname }}
+        </v-list-item-content>
+        <v-list-item-content>
+          {{ item.distro }}
+        </v-list-item-content>
+        <v-list-item-content>
           {{ item._id }}
         </v-list-item-content>
         <v-list-item-action>
