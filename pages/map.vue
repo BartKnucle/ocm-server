ca28e33f5a3a70a93da1d01f2930f3b30097e74a
BartKnucle <sjarde@yahoo.fr>
2019-11-19 18:40:00 +0100

    Added link and subnets

--- pages/map.vue
+++ pages/map.vue
@@ -4,12 +4,24 @@
       <circle
         v-for="device in devices().data"
         :key="device._id"
-        cx="500"
-        cy="500"
         r="20"
         :class="'device ' + device.distro.replace(/\s/g, '')"
       />
+      <circle
+        v-for="subnet in subnets().data"
+        :key="subnet._id"
+        r="20"
+        class="subnet"
+      />
+      <line
+        v-for="link in links"
+        :key="link.source + link.target"
+        stroke="white"
+        stroke-width="2"
+      />
     </svg>
+    {{ nodes }}
+    {{ links }}
   </section>
 </template>
 
@@ -25,35 +37,50 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('devices', { devices: 'find' })
+    ...mapGetters('devices', { devices: 'find' }),
+    ...mapGetters('subnets', { subnets: 'find' }),
+    nodes () {
+      const nodes = [...this.devices().data, ...this.subnets().data]
+      return nodes
+    },
+    links () {
+      const devicesLinks = [...this.devices().data]
+        .filter(link => link.gatewayV4)
+        .map((link) => {
+          const newLink = {}
+          newLink.source = link._id
+          newLink.target = link.gatewayV4
+          return newLink
+        })
+      return devicesLinks
+    }
   },
   created () {
-    this.findDevices()
+    const loadDevices = this.findDevices()
+    const loadSubnets = this.findSubnets()
+
+    Promise.all([loadDevices, loadSubnets])
       .then((devices) => {
         this.createGraph()
       })
   },
   methods: {
     ...mapActions('devices', { findDevices: 'find' }),
+    ...mapActions('subnets', { findSubnets: 'find' }),
     createGraph () {
       this.svg = d3.select('#graph')
         .attr('viewBox', `0 0 ${this.height} ${this.width}`)
 
-      this.simulation = d3.forceSimulation(
-        this.devices().data.map((device) => {
-          device.x = null
-          device.y = null
-          return device
-        })
-      )
-        .force('charge', d3.forceManyBody())
+      this.simulation = d3.forceSimulation(this.nodes)
+        .force('charge', d3.forceManyBody().strength(d => -100))
+        .force('link', d3.forceLink(this.links).id((d) => { return d._id }).distance(50))
         .force('center', d3.forceCenter(this.width / 2, this.height / 2))
         .on('tick', this.ticked)
     },
     ticked () {
       const u = this.svg
         .selectAll('circle')
-        .data(this.devices().data)
+        .data(this.nodes)
 
       u.enter()
         .merge(u)
@@ -65,6 +92,27 @@ export default {
         })
 
       u.exit().remove()
+
+      const v = this.svg
+        .selectAll('line')
+        .data(this.links)
+
+      v.enter()
+        .merge(v)
+        .attr('x1', (d) => {
+          return d.source.x
+        })
+        .attr('y1', (d) => {
+          return d.source.y
+        })
+        .attr('x2', (d) => {
+          return d.target.x
+        })
+        .attr('y2', (d) => {
+          return d.target.y
+        })
+
+      v.exit().remove()
     }
   }
 }
@@ -80,6 +128,11 @@ export default {
 
   .device {
     fill: blue;
+    stroke: white;
+  }
+
+  .device {
+    fill: red;
     stroke: white;
   }
 
