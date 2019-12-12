fea0f1fe60756e1f7c62d3a9b036ee2693d6443c
BartKnucle <sjarde@yahoo.fr>
2019-12-12 11:46:58 +0100

    Add multipart support upload

--- server/services/upload/upload.service.js
+++ server/services/upload/upload.service.js
@@ -1,10 +1,20 @@
+const multer = require('multer')
 const blobService = require('feathers-blob')
 const fs = require('fs-blob-store')
 const hooks = require('./upload.hooks')
+const multipartMiddleware = multer()
 
 module.exports = (app) => {
-  const blobStorage = fs(__dirname)
-  app.use('/api/upload', blobService({ Model: blobStorage }))
+  const blobStorage = fs(app.get('data'))
+  app.use(
+    '/api/upload',
+    multipartMiddleware.single('uri'),
+    (req, res, next) => {
+      req.feathers.file = req.file
+      next()
+    },
+    blobService({ Model: blobStorage })
+  )
   const service = app.service('/api/upload')
 
   service.hooks(hooks)
