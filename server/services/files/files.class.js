86bd9e28d151ced11cc7d319e1d5df433a3a95bf
BartKnucle <sjarde@yahoo.fr>
2020-01-17 11:04:25 +0100

    Add clear

--- server/services/files/files.class.js
+++ server/services/files/files.class.js
@@ -1,3 +1,36 @@
 const ServiceClass = require('../service.class')
+const fs = require('fs')
+const path = require('path')
 
-exports.Files = class Files extends ServiceClass {}
+
+exports.Files = class Files extends ServiceClass {
+  setup (app) {
+    super.setup(app)
+    this.clear()
+  }
+
+  // Sync files and database
+  clear() {
+    fs.readdir(path.join(this.app.get('homePath'), '/files/'), (err, files) => {
+      files.map((file) => {
+        //  Remove local file
+        this.get(file)
+          .catch(() => {
+            fs.unlink(path.join(this.app.get('homePath'), '/files/' + file), (err) => {})
+          })
+      })
+    })
+
+    //  Remove database record if the file is not present
+    this.find()
+      .then((files) => {
+        files.data.map((file) => {
+          fs.exists(path.join(this.app.get('homePath'), '/files/' + file._id), (exist) => {
+            if (!exist) {
+              this.remove(file._id)
+            }
+          })
+        })
+      })
+  }
+}
