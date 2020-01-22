99d46624c1d2c2b673f5f9ae74b05c46ecfa1215
Kaas <kaas@opencm.duckdns.org>
2020-01-22 16:54:00 +0100

    Fix lint

--- server/services/devicesDps/devicesDps.hooks.js
+++ server/services/devicesDps/devicesDps.hooks.js
@@ -1,8 +1,8 @@
-const { authenticate } = require('@feathersjs/authentication')
+//  const { authenticate } = require('@feathersjs/authentication')
 
 module.exports = {
   before: {
-    //all: [authenticate('jwt', 'api-key')],
+    //  all: [authenticate('jwt', 'api-key')],
     all: [],
     find: [],
     get: [],
